a32a8e3a04324a416803668968ae47621c9b0c1d
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-12 12:14:20 +0300

    added a function on js file for contact form with varriables and statements to pick and return users input and pop up an alert to the usser

--- js/script.js
+++ js/script.js
@@ -109,4 +109,18 @@ $(document).ready(function(){
 
 });
 
+// contact form
+
+
+function contactForm(){
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var message=document.getElementById('message').value;
+
+    if (name !== "" && email !== "" && message !== ""){
+        alert(name+ ' we have received your message. Thank you for reaching out to us.');
+    } else if(name !== "" || email !== "" || message !== ""){
+        alert('Fill everything, please.')
+    }
+};
 
