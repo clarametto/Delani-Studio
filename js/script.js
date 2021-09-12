8525ab871dd912aa6327e6e84a7f5ea7aaa7c88d
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-12 11:26:15 +0300

    Modified about us page and added atogle eefect on script .js file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+// WHAT WE DO
+
+$(document).ready(function(){
+    $(".Design").click(function(){
+        $("#design").toggle(2000);
+        $("#text").toggle(2000);
+    });
+    $(".twoDevelopment").click(function(){
+        $("#wwd2").toggle();
+        $("#twoDev").toggle();
+    });
+    $(".threeProductMG").click(function(){
+        $("#wwd3").toggle();
+        $("#threeProd").toggle();
+    });
+});
+
+
